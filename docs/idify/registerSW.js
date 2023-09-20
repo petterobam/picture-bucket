746a13d08087b57c63fb5ac1d3cdce806d3a6dda
欧阳Boy_Petter <1460300366@qq.com>
2023-09-20 11:30:11 +0800

    Update registerSW.js

--- docs/idify/registerSW.js
+++ docs/idify/registerSW.js
@@ -1,1 +1,9 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js')
+    .then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(function(error) {
+      console.log('Service Worker registration failed:', error);
+    });
+})}
